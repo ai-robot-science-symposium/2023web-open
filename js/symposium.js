94e6cfcec642acceea854be431fd1682db352678
Marie <dmasubuchi@live.jp>
2023-06-02 20:23:57 +0900

    ADD
    ADD

---
+++ js/symposium.js
@@ -0,0 +1,24 @@
+//////////// SPバーガーメニュー ////////////////////////////////////
+$(".openbtn1").click(function () {//ボタンがクリックされたら
+  $(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn1").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスも除去
+});
+
+
+
+
+//////////// モーダル ////////////////////////////////////
+$(".info").modaal({
+  overlay_close:true,//モーダル背景クリック時に閉じるか
+  before_open:function(){// モーダルが開く前に行う動作
+    $('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+  },
+  after_close:function(){// モーダルが閉じた後に行う動作
+    $('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+  }
+});
